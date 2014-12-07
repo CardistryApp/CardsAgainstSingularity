1765193dff56be55a7c6913525e5f26d22d0293b
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2014-12-07 15:25:01 -0500

    great progress so far

--- src/app/index.js
+++ src/app/index.js
@@ -3,15 +3,31 @@
 angular.module('cardistry', ['ngCookies', 'ngTouch', 'ui.router', 'firebase',
 	'cardistry.cards','cardistry.main'
 	])
+
+  // .run(function($rootScope, $state) {
+  //   $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+  //     if (error === "AUTH_REQUIRED") {
+  //       alert("please log in!")
+  //       $state.go("home");
+  //     }
+  //   })
+  // })
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: "app/partials/home.html"
+        templateUrl: "app/partials/home.html",
+        controller: "MainCtrl as app"
       })
       .state('account', {
+        controller: "MainCtrl as app",
         url: '/account',
-        templateUrl: "app/partials/account-page.html"
+        templateUrl: "app/partials/account-page.html",
+        // resolve: {
+        //   "currentAuth": ["Auth", function(Auth) {
+        //     return Auth.$requireAuth();
+        //     }]
+        //   }
       })
       .state('player', {
         url: '/player',
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,111 +2,67 @@
 
 angular.module('cardistry.main', ['cardistry.cards','firebase'])
 
-  .controller('MainCtrl', function (Deck, $scope, $firebase) {
+  .controller('MainCtrl', function (Deck, $scope, $firebase, Auth) {
+  	
+  	//firebase gameObject stuff
   	var ref = new Firebase('https://cardistry.firebaseio.com').child('gameDB')
   	var sync = $firebase(ref);
   	var syncObject = sync.$asObject();
 
-  	syncObject.$bindTo($scope, 'gameDB');
+  	//firebase profile stuff
+  	var pref = new Firebase('https://cardistry.firebaseio.com').child('gameDB').child('players')
+  	var psync = $firebase(pref)
+  	var psyncArray = psync.$asArray();
+  	var psyncObj = psync.$asObject();
 
-  	this.sanityTest = function(){
-  		syncObject.test = "Wowie wow wow"
+  	//Auth stuff
+  	$scope.auth = Auth;
+    $scope.logIn = function(){
+      $scope.auth.$authWithOAuthPopup("facebook").then(function(authData) {
+        console.log("Logged in as:", authData.facebook.displayName);
+
+      }).catch(function(error) {
+        console.error("Authentication failed: ", error);
+      });
+    }
+
+    function isNewUser() {
+       pref.child($scope.user.uid).once('value', function(snapshot) {
+         if(snapshot.val() !== null){
+         	return false
+         }
+       })
+     }
+
+    $scope.user = $scope.auth.$getAuth();
+		
+		pref.onAuth(function(authData) {
+  		if (authData && isNewUser) {
+  			$scope.user.firstName = $scope.user.facebook.cachedUserProfile.first_name,
+  			$scope.user.profilePic = $scope.user.facebook.cachedUserProfile.picture.data.url,
+  			psyncObj.$save()
+    		pref.child(authData.uid).set(authData);
+ 			 }
+		});
+
+
+  	console.log($scope.user.uid)
+
+  	var Player = function(){
+  		return {
+  			firstName: $scope.user.facebook.cachedUserProfile.first_name,
+  			profilePic: $scope.user.facebook.cachedUserProfile.picture.data.url
+  		}
   	}
 
+	  
+	  //locking the viewport	
   	$('html, body').css({'overflow': 'hidden','height': '100%'})
 
+})
 
-
-
-});
-
- //  	var gameObject = FBobj();
- //  	console.log(gameObject)
-
- //  	this.fbPlayList = function(){
- //  		console.log(gameObject.game.players)
- //  	}
-
-	// 	var game = gameObject.game = new Game();
-	// 	$cookieStore.put('game', game)
-	// 	gameObject.game = $cookieStore.get('game')
-	// 	gameObject.$save()
-
-	// 	this.firebaseSync = function() {
-	// 		console.log("saved")
-	// 		gameObject.$save()
-	// 	}
-	// 	this.syncUp = function(){
-	// 		this.firebaseSync();
-	// 		$state.go('player');
-	// 	}
-	// 	this.playCard = function(player, cardId, cardText, index){
-	// 		game.turn.cardsPlayed.push({
-	// 						player: $rootScope.player.name,	
-	// 						cardId: cardId,
-	// 						cardText: cardText
-	// 					})
-	// 		$rootScope.player.cards.splice(index, 1);
-	// 		$('li#'+index).remove();
-	// 		gameObject.game = $scope.game
-	// 		console.log(game)
-	// 		this.firebaseSync();
-	// }
- //  	var self = this;
-
- //  	// this.player = $cookieStore.get('player')
-
- //  	this.addPlayer = function(name) {
-	// 		self.name = name
-	// 		var player = new Player(name)
-	// 		game.players.push(player)
-	// 		// $cookieStore.put('player', player)
-	// 		$state.go('hand', {id: player.id});
-	// 		$rootScope.player = player
-	// 		gameObject.$save()
-	// 		return false
-	// 	}
-	// 	this.log = function(){
-	// 		console.log(this.player)
-	// 	}
-
-	// 	var Player = function(name, index){
-	// 		this.name = name;
-	// 		this.cards = $filter('limitTo')(game.deck.whiteCards, 10);
-	// 		this.id = game.players.length + 1;
-	// 		this.score = 0;
-	// 		game.deck.whiteCards.splice(index, 10);
-	// 	}
-	
-	// $rootScope.game = game
- //  })
-
-	// .controller('PlayerCtrl', function($rootScope, $stateParams, $cookieStore){
-	// 	this.game = $rootScope.game
-	// 	console.log(this.game)
-	// 	this.player = $rootScope.player
-	// })
-
-	// .factory('FBobj', function($firebase){
-	// 	return function(gameId){
-	// 		var ref = new Firebase("https://cardistry.firebaseio.com/").child('games')
-	// 		return $firebase(ref).$asObject();
-	// 	}
-	// })
-
-	// .factory('Game', function(Deck){
-	// 	return function(){
-	// 		this.deck = {
-	// 			whiteCards: Deck.whiteCards,
-	// 			blackCards: Deck.blackCards
-	// 		}
-	// 		this.finalScore = 10,
-	// 		this.turn = {
-	// 			number: 0,
-	// 			currentDealer: "",
-	// 			cardsPlayed: []
-	// 		}
-	// 			this.players = []
-	// 		}
-	// });
+  .factory('Auth', function($firebaseAuth){
+		var ref = new Firebase('https://cardistry.firebaseio.com').child('gameDB')
+  	return $firebaseAuth(ref);
+  })
 
