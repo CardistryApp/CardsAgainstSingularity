3b9d3b35a6bdb8b6d6523ef3a3ca49d33fbd1acf
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2014-12-16 11:22:33 -0500

    cards in dictionaries

--- src/app/scripts/cardCtrl.js
+++ src/app/scripts/cardCtrl.js
@@ -2,6 +2,41 @@
 
 angular.module('cardistry.cards', [])
 
+  .factory('Deck2', function($http){
+    var service = {
+      load: function(){
+        return $http.get('/assets/data/cards.JSON').then(function(deck){
+          service.cards = {};
+          service.cards.white = {}
+          service.cards.black = {}
+          var cards = deck.data
+          for(var i = 0; i < cards.length; i++)
+            if(cards[i].cardType === "A"){
+            {
+              service.cards.white[cards[i].id] = {
+                text: cards[i].text,
+                priority: 0,
+                type: cards[i].cardType,
+                expansion: cards[i].expansion
+              }
+            }
+          } else {
+            {
+              service.cards.black[cards[i].id] = {
+                text: cards[i].text,
+                priority: 0,
+                type: cards[i].cardType,
+                expansion: cards[i].expansion
+              }
+            }
+          }
+        }) //END PROMISE RETURN
+      } // END LOAD
+    } //END SERVICE
+    return service;
+  })
+
+
 	.service('Deck', function($http){
 
   	//creating the deck//
